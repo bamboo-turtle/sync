cd3789770cd744fd34b05c6fdc3027fee9613220
Eugene Bolshakov <eugene.bolshakov@gmail.com>
2019-03-04 14:48:22 +0000

    Add serverless framework config for deployment
    Added a serverless config to deploy the app as 3 AWS lambda functions:
    
    * Function to get all products from Airtable and trigger their sync
    * Function to sync a simple product
    * Function to sync a variable product
    
    The first function runs daily at midnight for now and triggers the other
    two using AWS SNS.
    
    AWS Lambda uses Ruby 2.5.0, so updated the local setup accordingly.

---
+++ hander.rb
@@ -0,0 +1,56 @@
+require "lib/product"
+require "lib/category"
+require "lib/variable_product"
+require "lib/synchroniser"
+require "aws-sdk-sns"
+require "json"
+
+SIMPLE_PRODUCT_TOPIC = "sync-simple-product"
+VARIABLE_PRODUCT_TOPIC = "sync-variable-product"
+
+def sync_products(event:, context:)
+  airtable = Airtable::Store.new(
+    database_id: ENV["AIRTABLE_DATABASE"],
+    api_key: ENV["AIRTABLE_API_KEY"]
+  )
+
+  sns = Aws::SNS::Client.new
+  topics = sns.list_topics.topics.map { |t|
+    [
+      t.topic_arn.split(":").last,
+      t.topic_arn
+    ]
+  }.to_h
+
+  variable, simple = airtable.products.partition(&:variant)
+
+  VariableProduct.map(variable).each do |product|
+    sns.publish(
+      topic_arn: topics.fetch(VARIABLE_PRODUCT_TOPIC),
+      message: { ids: product.airtable_ids }.to_json,
+    )
+  end
+
+  simple.each do |product|
+    sns.publish(
+      topic_arn: topics.fetch(SIMPLE_PRODUCT_TOPIC),
+      message: { id: product.airtable_id }.to_json,
+    )
+  end
+
+  nil
+end
+
+def sync_simple_product(event:, context:)
+  event["Records"].each do |record|
+    id = JSON.parse(record["Sns"]["Message"])["id"]
+    Synchroniser.synchronise_simple_product(id)
+  end
+end
+
+def sync_variable_product(event:, context:)
+  event["Records"].each do |record|
+    ids = JSON.parse(record["Sns"]["Message"])["ids"]
+    Synchroniser.synchronise_variable_product(ids)
+  end
+end
